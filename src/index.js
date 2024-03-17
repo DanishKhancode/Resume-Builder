bd4511aaa615bfb7c873b3dd25e6a04f398be400
Danish <khandanish36408@gmail.com>
2024-03-17 15:38:20 +0530

    safe

--- src/index.js
+++ src/index.js
@@ -14,14 +14,7 @@ import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCVrMsUlKLoyn1U9UP1q4R9piPvMUsnkMc",
-  authDomain: "resumebuilder-fjp5.firebaseapp.com",
-  projectId: "resumebuilder-fjp5",
-  storageBucket: "resumebuilder-fjp5.appspot.com",
-  messagingSenderId: "471582927327",
-  appId: "1:471582927327:web:d317c1f7cc6b1969bafc6a",
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
